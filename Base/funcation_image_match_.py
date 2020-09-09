eb5f8624c127687fa0e16999aa4e3ec3ca3f62ac
git-2676563876 <2676563876@qq.com>
2020-09-09 20:21:26 +0800

    微调

--- Base/funcation_image_match_.py
+++ Base/funcation_image_match_.py
@@ -1,4 +1,19 @@
 from pymouse import  PyMouse
 from pykeyboard import PyKeyboard
+from Base.Image_match import ImageMath
 import os
 import time
+
+class Image_Test():
+    def __init__(self):
+        self.mouse=PyMouse()
+        self.keyboard=PyKeyboard()
+        self.match=ImageMath()
+
+    def start_app(self):
+        pass
+
+    def on_double(self,target,n=1):
+        x,y=ImageMath().find_image(target)
+        self.mouse.click(x,y,n=n)
+        print('在%d，%d位置单机%d次' % ( x,y,n ))
